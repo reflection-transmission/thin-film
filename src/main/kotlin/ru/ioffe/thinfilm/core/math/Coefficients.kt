972ba18a50e9496211c1e9843e38db8e98772d65
Nikifor Fedorov <nikifor.fedorov@arsysop.ru>
2021-10-12 13:48:32 +0300

    removed unused class, renamed odelevsky formula

--- src/main/kotlin/ru/ioffe/thinfilm/core/math/Coefficients.kt
+++
@@ -1,3 +0,0 @@
-package ru.ioffe.thinfilm.core.math
-
-data class Coefficients(val transition: Double, val reflection: Double)
--- src/main/kotlin/ru/ioffe/thinfilm/core/math/OdelevskyFormula.kt
+++ src/main/kotlin/ru/ioffe/thinfilm/core/math/OdelevskyFormula.kt
@@ -3,7 +3,7 @@ package ru.ioffe.thinfilm.core.math
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-class Odelevsky {
+class OdelevskyFormula {
 
     fun apply(e1: Double, e2: Double, volume: Double): Double {
         val a = (volume - 0.5) * (e1 - e2)
