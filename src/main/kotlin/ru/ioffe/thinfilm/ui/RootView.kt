5a513933d9d6fdab16a9f3c53a29cc65bd2547d9
Nikifor Fedorov <nikifor.fedorov@arsysop.ru>
2021-10-12 13:51:22 +0300

    removed unused menu entries

--- src/main/kotlin/ru/ioffe/thinfilm/ui/RootView.kt
+++ src/main/kotlin/ru/ioffe/thinfilm/ui/RootView.kt
@@ -11,16 +11,8 @@ class RootView : View() {
 
     override val root = vbox {
         menubar {
-            menu("File") {
-                item("New", KeyCombination.valueOf("Ctrl+N")).action { println("new") }
-                item("Save", KeyCombination.valueOf("Ctrl+S")).action { println("saved") }
-                item("Open", KeyCombination.valueOf("Ctrl+O")).action { println("opened") }
-            }
             menu("Library") {
                 item("Materials", KeyCombination.valueOf("Ctrl+L")).action { openInternalWindow(LibraryView(registry)) }
-            }
-            menu("Help") {
-                item("About") { println("about") }
             }
         }
         borderpane {
