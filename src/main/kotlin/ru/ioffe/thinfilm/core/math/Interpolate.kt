ba59ef93e89ef81a7c5f0fe4c9489fb61d1e5df4
Nikifor Fedorov <nikifor.fedorov@arsysop.ru>
2021-10-21 12:35:20 +0300

    fixed interpolation

--- src/main/kotlin/ru/ioffe/thinfilm/core/math/Interpolate.kt
+++ src/main/kotlin/ru/ioffe/thinfilm/core/math/Interpolate.kt
@@ -4,40 +4,18 @@ import org.apache.commons.math3.analysis.interpolation.SplineInterpolator
 
 class Interpolate {
 
-    fun value(x: DoubleArray, y: DoubleArray, wavelength: Double): Double {
-        val interpolator = SplineInterpolator()
+    fun value(xs: DoubleArray, ys: DoubleArray, wavelength: Double): Double {
         return try {
-            val closest = closest(wavelength, x.asList())
-            val function =
-                interpolator.interpolate(
-                    x.copyOfRange(closest - 2, closest + 2),
-                    y.copyOfRange(closest - 2, closest + 2)
-                )
+            val closest = Closest(xs).forValue(wavelength)
+            val function = SplineInterpolator().interpolate(
+                xs.copyOfRange(closest - 2, closest + 2),
+                ys.copyOfRange(closest - 2, closest + 2)
+            )
             function.value(wavelength)
         } catch (e: Exception) {
+            println(e)
             0.0
         }
-    }
-
-    private fun closest(value: Double, wavelengths: List<Double>): Int {
-        if (value < wavelengths.first() || value > wavelengths.last()) {
-            throw Exception("Out of bounds. (${wavelengths.first()}, ${wavelengths.last()}) does not include $value")
-        }
-        return search(value, wavelengths)
-    }
-
-    private fun search(value: Double, wavelengths: List<Double>): Int {
-        var low = 0
-        var high = wavelengths.size - 1
-        var mid = 0
-        while (low < high) {
-            mid = (low + high) / 2
-            val midValue = wavelengths[mid]
-            if (midValue == value) return mid
-            else if (midValue > value) low = mid + 1
-            else high = mid - 1
-        }
-        return mid
     }
 
 }
