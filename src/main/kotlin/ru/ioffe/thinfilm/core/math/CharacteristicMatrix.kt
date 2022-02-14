a0a16fe77f721fca2ddbd762b84adf1d7c9cd184
Nikifor Fedorov <nikifor.fedorov@arsysop.ru>
2022-02-14 17:04:54 +0300

    second week
    algo switch wip

--- src/main/kotlin/ru/ioffe/thinfilm/core/math/CharacteristicMatrix.kt
+++ src/main/kotlin/ru/ioffe/thinfilm/core/math/CharacteristicMatrix.kt
@@ -1,23 +1,31 @@
 package ru.ioffe.thinfilm.core.math
 
 import org.apache.commons.math3.complex.Complex
+import org.apache.commons.math3.linear.FieldMatrix
+import org.apache.commons.math3.linear.MatrixUtils
 import ru.ioffe.thinfilm.core.model.Layer
 import kotlin.math.*
 
 class CharacteristicMatrix(private val layer: Layer) {
 
-    fun calculate(wavelength: Double) {
+    fun calculate(wavelength: Double): FieldMatrix<Complex> {
         val m11 = Complex(
             cos(phi(wavelength)) * cosh(phiPrime(wavelength)),
             sin(phi(wavelength)) * sinh(phiPrime(wavelength))
         )
         val m12 = Complex(
-            n(wavelength) * sin(phi(wavelength)) * cosh(phiPrime(wavelength))
+            (n(wavelength) * paramA(wavelength) + k(wavelength) * paramB(wavelength)) / paramC(wavelength),
+            (k(wavelength) * paramA(wavelength) - n(wavelength) * paramB(wavelength)) / paramC(wavelength)
+        )
+        val m21 = Complex(
+            n(wavelength) * paramA(wavelength) - k(wavelength) * paramB(wavelength),
+            -k(wavelength) * paramA(wavelength) - n(wavelength) * paramB(wavelength)
         )
         val m22 = Complex(
             cos(phi(wavelength)) * cosh(phiPrime(wavelength)),
             sin(phi(wavelength)) * sinh(phiPrime(wavelength))
         )
+        return MatrixUtils.createFieldMatrix(arrayOf(arrayOf(m11, m12), arrayOf(m21, m22)))
     }
 
     private fun phi(wavelength: Double): Double = 2 * PI * n(wavelength) * depth() / lambda(wavelength)
@@ -32,4 +40,9 @@ class CharacteristicMatrix(private val layer: Layer) {
 
     private fun depth(): Double = layer.depth * 10.0.pow(-9)
 
+    private fun paramA(wavelength: Double): Double = sin(phi(wavelength)) * cosh(phiPrime(wavelength))
+
+    private fun paramB(wavelength: Double): Double = cos(phi(wavelength)) * sinh(phiPrime(wavelength))
+
+    private fun paramC(wavelength: Double) = n(wavelength).pow(2) + k(wavelength).pow(2)
 }
--- src/main/kotlin/ru/ioffe/thinfilm/core/model/Layer.kt
+++ src/main/kotlin/ru/ioffe/thinfilm/core/model/Layer.kt
@@ -2,9 +2,9 @@ package ru.ioffe.thinfilm.core.model
 
 import org.apache.commons.math3.complex.Complex
 import org.apache.commons.math3.linear.FieldMatrix
-import org.apache.commons.math3.linear.MatrixUtils
+import ru.ioffe.thinfilm.core.math.CharacteristicMatrix
 import ru.ioffe.thinfilm.net.MaterialProperties
-import kotlin.math.*
+import kotlin.math.pow
 
 class Layer(
     val properties: MaterialProperties = MaterialProperties.Constant(1.0),
@@ -13,42 +13,6 @@ class Layer(
     private val fulfill: Double = 1.0
 ) {
 
-    private fun d() = depth * 10.0.pow(-9)
-
-    fun m(wavelength: Double): FieldMatrix<Complex> {
-        val n = properties.n(wavelength) // complex refractive index
-        val k = properties.k(wavelength)
-        val lambda = wavelength * 10.0.pow(-6)
-        val a = Complex(
-            -sin(2 * PI * n * d() / lambda) * sinh(k * d()),
-            cos(2 * PI * n * d() / lambda) * cosh(k * d())
-        )
-        val b = Complex(
-            -(k * a(n, lambda, k) - n * b(n, lambda, k)),
-            n * a(n, lambda, k) + k * b(n, lambda, k)
-        )
-        val c = Complex(
-            -(k * a(n, lambda, k) - n * b(n, lambda, k)),
-            n * a(n, lambda, k) - k * b(n, lambda, k)
-        )
-        return MatrixUtils.createFieldMatrix(arrayOf(arrayOf(a, b), arrayOf(c, a)))
-    }
-
-    private fun b(n: Double, lambda: Double, k: Double) = cos(2 * PI * n * d() / lambda) * sinh(k * d())
-
-    private fun a(n: Double, lambda: Double, k: Double): Double = sin(2 * PI * n * d() / lambda) * cosh(k * d())
-
-
-    operator fun Complex.times(value: Double): Complex = this.multiply(value)
-
-    operator fun Complex.times(value: Complex): Complex = this.multiply(value)
-
-    operator fun Complex.plus(value: Complex): Complex = this.add(value)
-
-    operator fun Complex.minus(value: Complex): Complex = this.subtract(value)
-
-    operator fun Complex.div(value: Complex): Complex = this.divide(value)
-
-    operator fun Complex.div(value: Double): Complex = this.divide(value)
+    fun m(wavelength: Double): FieldMatrix<Complex> = CharacteristicMatrix(this).calculate(wavelength)
 
 }
